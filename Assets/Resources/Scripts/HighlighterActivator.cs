797da0ddb4c25f79ab5e28affc64db38faeca956
Weslo <wesley.rockholz@gmail.com>
2017-07-08 12:34:53 -0400

    Add assets, plugins, test scripts

---
+++ Assets/Resources/Scripts/HighlighterActivator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using HighlightingSystem;
+
+namespace SubwaySpraypainter {
+
+	// Automatically activates a Highlighter component.
+	[RequireComponent(typeof(Highlighter))]
+	public class HighlighterActivator : MonoBehaviour {
+
+		// Self-initialize.
+		void Start() {
+			GetComponent<Highlighter>().ConstantOnImmediate(Color.red);
+		}
+	}
+}
